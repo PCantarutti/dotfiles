fc51c4d64a68988860b3b43a9f41aabe264b28f7
pedro_cantarutti <pedrocantarutti022@gmail.com>
2026-03-16 03:56:02 -0300

    ignorando cache e logs do VSCode

--- Code/User/History/aaa3ab/fALw.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-devices=$(bluetoothctl devices | awk '{print $3}')
-chosen=$(echo -e "$devices" | rofi -dmenu -p "Bluetooth")
-[ -z "$chosen" ] && exit
-bluetoothctl connect $(bluetoothctl devices | grep "$chosen" | awk '{print $2}')
